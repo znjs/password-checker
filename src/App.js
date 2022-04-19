7b32d5242e44d04d56f14893f4346b6d55623f22
znjs <jasrikar@gmail.com>
2022-04-19 21:08:53 +0530

    feat: strength checker

--- src/App.js
+++ src/App.js
@@ -1,17 +1,51 @@
 import { useState } from "react";
 import "./App.css";
+import { strengthCheck } from "./strength";
 
 function App() {
   const [password, setPassword] = useState("");
+  // const [strength, setStrength] = useState(0);
+  strengthCheck("Srikar@1234");
   return (
     <div className="App">
       <h1>Password Checker</h1>
-      <input
-        type="text"
-        placeholder="Password..."
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
+      <p>{strengthCheck(password)}</p>
+      <div>
+        <div>
+          <label>Password:</label>
+          <input
+            type="text"
+            placeholder="Password..."
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+
+        <div
+          style={{
+            width: "10rem",
+            height: "0.8rem",
+            backgroundColor: "gray",
+            display: "inline-block",
+            borderRadius: "2rem",
+            overflow: "hidden",
+          }}
+        >
+          <div
+            style={{
+              width: `${strengthCheck(password)}%`,
+              height: "100%",
+              backgroundColor: `${
+                strengthCheck(password) > 70
+                  ? "green"
+                  : strengthCheck(password) > 30
+                  ? "orange"
+                  : "red"
+              }`,
+            }}
+          ></div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/strength.js
@@ -0,0 +1,52 @@
+const strengthCheck = (password) => {
+  let strength = 0;
+  let upperCaseCount = 0;
+  let lowerCaseCount = 0;
+  let numberCount = 0;
+  let specialCharCount = 0;
+  if (password.length > 6 && password.length < 32) {
+    for (let i in password) {
+      let charCode = password[i].charCodeAt(0);
+      switch (true) {
+        case charCode >= 97 && charCode <= 122:
+          lowerCaseCount++;
+          break;
+        case charCode >= 65 && charCode <= 90:
+          upperCaseCount++;
+          break;
+        case charCode >= 48 && charCode <= 57:
+          numberCount++;
+          break;
+        case (charCode > 33 && charCode <= 47) ||
+          (charCode >= 58 && charCode <= 64) ||
+          (charCode >= 91 && charCode <= 96) ||
+          (charCode >= 123 && charCode <= 126):
+          specialCharCount++;
+          break;
+        default:
+          break;
+      }
+    }
+  }
+  if (specialCharCount) strength++;
+  if (lowerCaseCount) strength++;
+  if (upperCaseCount) strength++;
+  if (numberCount) strength++;
+  if (specialCharCount > 2) strength += 2;
+  if (lowerCaseCount > 2) strength += 2;
+  if (upperCaseCount > 2) strength += 2;
+  return strength > 10 ? 100 : strength * 10;
+};
+
+export { strengthCheck };
+
+// ?CHAR CODES
+/**
+ * a - 97
+ * A - 65
+ * z - 122
+ * Z - 90
+ * 0 - 48
+ * 9 - 57
+ * special Chars - 33-47/58-64/91-96/123-126
+ */
